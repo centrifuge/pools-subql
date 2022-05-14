6221084c548c0a6aa7008e09f482fa9d81bb4b6c
Jeroen Offerijns <jeroen@offerijns.com>
2022-05-14 22:47:27 +0200

    Handle anonymous proxy creation

--- src/mappings/proxies.ts
+++ src/mappings/proxies.ts
@@ -1,11 +1,29 @@
 import { SubstrateEvent } from '@subql/types'
+import { Proxy, AnonymousProxy } from '../types'
 
 export async function handleProxyAdded(event: SubstrateEvent): Promise<void> {
   logger.info(`Proxy added: ${event.toString()}`)
 
-  // const proxy = new Proxy(`${event.delegator.toString()}-${epochId.toString()}`)
-  // await proxy.save()
+  const [delegator, delegatee, proxyType, delay] = event.event.data
+
+  const proxy = new Proxy(`${delegator.toString()}-${delegatee.toString()}`)
+  proxy.delegator = delegator.toString()
+  proxy.delegatee = delegatee.toString()
+  proxy.proxyType = proxyType.toString()
+  proxy.delay = Number(delay.toString())
+  await proxy.save()
+}
+
+export async function handleProxyAnonymousCreated(event: SubstrateEvent): Promise<void> {
+  logger.info(`Anonymous proxy created: ${event.toString()}`)
+
+  const [account, createdBy, proxyType, _index] = event.event.data
+
+  const proxy = new AnonymousProxy(`${account.toString()}`)
+  proxy.account = account.toString()
+  proxy.createdBy = createdBy.toString()
+  proxy.proxyType = proxyType.toString()
+  await proxy.save()
 }
 
 // TODO: handleProxyRemoved
-// TODO: handleProxyAnonymousCreated
