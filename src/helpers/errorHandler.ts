20f032e0ba23aacde26d25e4d3ce4ddd39da000c
Filippo Fontana <filippo@embrio.tech>
2022-06-08 14:50:24 +0200

    improved documentation

--- src/helpers/errorHandler.ts
+++ src/helpers/errorHandler.ts
@@ -1,6 +1,11 @@
 type InferArgs<T> = T extends (...t: [...infer Arg]) => any ? Arg : never
 type InferReturn<T> = T extends (...t: [...infer Arg]) => infer Res ? Res : never
 
+/**
+ * Wrapper to provide standard error handling throughout the application
+ * @param method Block handler, Event handler or function for witch a generic catcher should be attached
+ * @returns Same function with a corresponding ceneric catcher for error handling
+ */
 export function errorHandler<TFunc extends (...args: any[]) => any>(
   method: TFunc
 ): (...args: InferArgs<TFunc>) => InferReturn<TFunc> {
--- src/helpers/timeKeeping.ts
+++ src/helpers/timeKeeping.ts
@@ -1,12 +1,15 @@
-import { SubstrateBlock } from "@subql/types"
-import { SNAPSHOT_INTERVAL_SECONDS } from "../config"
+import { SubstrateBlock } from '@subql/types'
+import { SNAPSHOT_INTERVAL_SECONDS } from '../config'
 
+/**
+ * Manages the in memory tracking of time and indexing of periods
+ */
 export class MemTimekeeper {
   constructor(lastPeriodStart: Date) {
     this._currentPeriodStart = lastPeriodStart
   }
   private _currentPeriodStart: Date = null
-  public getCurrentPeriod = ():Date => (this._currentPeriodStart)
+  public getCurrentPeriod = (): Date => this._currentPeriodStart
   public processBlock = (block: SubstrateBlock): boolean => {
     const blockPeriodStart = getPeriodStart(block.timestamp)
     const isNewPeriod = blockPeriodStart.valueOf() > this._currentPeriodStart.valueOf()
@@ -15,9 +18,13 @@ export class MemTimekeeper {
   }
 }
 
+/**
+ * Computes the start timestamp given an arbitrary block timestamb
+ * @param timestamp Arbitrary timestamp, usually from a block
+ * @returns Corresponding timestamp at the start of the period
+ */
 export function getPeriodStart(timestamp: Date): Date {
   const timestampSec = timestamp.valueOf() / 1000
-  const periodStartTimestampSec = timestampSec - timestampSec % SNAPSHOT_INTERVAL_SECONDS
+  const periodStartTimestampSec = timestampSec - (timestampSec % SNAPSHOT_INTERVAL_SECONDS)
   return new Date(periodStartTimestampSec * 1000)
-  
 }
