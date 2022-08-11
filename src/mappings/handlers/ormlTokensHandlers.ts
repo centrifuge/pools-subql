ce466c893c878e9f9382e4a070ad62fd33b3d929
Filippo Fontana <filippo@embrio.tech>
2022-08-11 15:31:28 +0200

    fix: investor transfer logic

--- src/mappings/handlers/ormlTokensHandlers.ts
+++ src/mappings/handlers/ormlTokensHandlers.ts
@@ -14,35 +14,41 @@ async function _handleTokenTransfer(event: SubstrateEvent): Promise<void> {
     const fromId = String.fromCharCode(...from.toU8a())
     const toId = String.fromCharCode(...to.toU8a())
 
+    // FILTER OUT ALL TRANSACTIONS CONTAINING A POOL ADDRESS
+    if (fromId.startsWith('pool') || toId.startsWith('pool')) return
+
     logger.info(
       `Token transfer tor tranche: ${poolId.toString()}-${trancheId.toString()}.
       from: ${fromId} to: ${toId} amount: ${amount.toString()}
       at block ${event.block.block.header.number.toString()}`
     )
 
-    const address = fromId.startsWith('pool') ? to : from
-
     // Get corresponding pool
     const pool = await PoolService.getById(poolId.toString())
 
-    const orderData: [string, string, number, string, string, bigint, Date] = [
+    // CREATE 2 TRANSFERS FOR FROM AND TO ADDRESS
+    // with from create TRANSFER_OUT
+    const txOut = InvestorTransactionService.transferOut(
       poolId.toString(),
       trancheId.toString(),
       pool.pool.currentEpoch,
-      address.toString(),
+      from.toString(),
       event.extrinsic.extrinsic.hash.toString(),
       amount.toBigInt(),
-      event.block.timestamp,
-    ]
+      event.block.timestamp
+    )
+    await txOut.save()
 
-    // if fromId starts with pool TRANSFER_OUT
-    if (fromId.startsWith('pool')) {
-      const tx = InvestorTransactionService.transferOut(...orderData)
-      await tx.save()
-    } else {
-      // else tTRANSFER_IN
-      const tx = InvestorTransactionService.transferIn(...orderData)
-      await tx.save()
-    }
+    // with to create TRANSFER_IN
+    const txIn = InvestorTransactionService.transferIn(
+      poolId.toString(),
+      trancheId.toString(),
+      pool.pool.currentEpoch,
+      to.toString(),
+      event.extrinsic.extrinsic.hash.toString(),
+      amount.toBigInt(),
+      event.block.timestamp
+    )
+    await txIn.save()
   }
 }
