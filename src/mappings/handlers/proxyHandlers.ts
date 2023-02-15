8116ff63c38e9f0284002a72b843b9f919345ecf
Jeroen Offerijns <jeroen@offerijns.com>
2023-02-15 15:52:54 +0100

    Fix creating accounts for pure proxies

--- src/mappings/handlers/proxyHandlers.ts
+++ src/mappings/handlers/proxyHandlers.ts
@@ -1,5 +1,6 @@
 import { SubstrateEvent } from '@subql/types'
 import { Proxy, PureProxy } from '../../types'
+import { AccountService } from '../services/accountService'
 
 export async function handleProxyAdded(event: SubstrateEvent): Promise<void> {
   logger.info(`Proxy added: ${event.toString()}`)
@@ -17,17 +18,20 @@ export async function handleProxyAdded(event: SubstrateEvent): Promise<void> {
 export async function handleProxyPureCreated(event: SubstrateEvent): Promise<void> {
   logger.info(`Pure proxy created: ${event.toString()}`)
 
-  const [account, createdBy, proxyType] = event.event.data
+  const [address, createdByAddress, proxyType] = event.event.data
+
+  const account = await AccountService.getOrInit(address.toString())
+  const createdBy = await AccountService.getOrInit(createdByAddress.toString())
 
   const pureProxy = new PureProxy(`${account.toString()}`)
-  pureProxy.accountId = account.toString()
-  pureProxy.createdBy = createdBy.toString()
+  pureProxy.accountId = account.id
+  pureProxy.createdBy = createdBy.id
   pureProxy.proxyType = proxyType.toString()
   await pureProxy.save()
 
-  const proxy = new Proxy(`${account.toString()}-${createdBy.toString()}-${proxyType.toString()}}`)
-  proxy.delegator = account.toString()
-  proxy.delegatee = createdBy.toString()
+  const proxy = new Proxy(`${account.id}-${createdBy.id}-${proxyType.toString()}}`)
+  proxy.delegator = account.id
+  proxy.delegatee = createdBy.id
   proxy.proxyType = proxyType.toString()
   await proxy.save()
 }
