502f98abfa73e9278ac93aedb15d19c0aa5a3492
Filippo <filippo@embrio.tech>
2023-03-20 12:26:43 +0100

    Reactivate handling of pool updates (#35)
    * chore: upgrade chain seed
    
    * chore: remove static project.yaml
    
    * fix: re-enable handling of pool updates
    
    * ci: test initialise project.yaml
    
    * fix: linting in proxyHandler

--- src/mappings/handlers/logHandlers.ts
+++ src/mappings/handlers/logHandlers.ts
@@ -4,6 +4,7 @@ import { errorHandler } from '../../helpers/errorHandler'
 export const logEvents = errorHandler(_logEvents)
 async function _logEvents(event: SubstrateEvent) {
   if (event.event.section === 'system') return
+  if (event.event.section === 'liquidityRewards') return
   logger.info(
     `Event method fired for pallet ${event.event.section}: ${event.event.method} ` +
       `at block ${event.block.block.header.number.toNumber()}`
