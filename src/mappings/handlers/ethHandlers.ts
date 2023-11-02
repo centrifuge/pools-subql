057f90682ef3b396b90f25d98673795684e5d504
Filippo Fontana <filippo@embriotech.ch>
2023-11-02 19:15:01 +0100

    feat: initialise multichain

--- src/mappings/handlers/ethHandlers.ts
+++ src/mappings/handlers/ethHandlers.ts
@@ -1,7 +1,17 @@
-import { SubstrateEvent } from '@subql/types'
+import { createTrancheTrackerDatasource } from '../../types'
 import { errorHandler } from '../../helpers/errorHandler'
+import { DeployTrancheLog } from '../../types/abi-interfaces/PoolManagerAbi'
+import { TransferLog } from '../../types/abi-interfaces/Erc20Abi'
 
-export const ethLogger = errorHandler(_ethLogger)
-async function _ethLogger(event: SubstrateEvent): Promise<void> {
-  logger.info(JSON.stringify(event))
+export const handleEvmDeployTranche = errorHandler(_handleEvmDeployTranche)
+async function _handleEvmDeployTranche(event: DeployTrancheLog): Promise<void> {
+  const [ poolId, trancheId, tokenAddress ] = event.args
+  logger.info(`Adding DynamicSource for pool ${poolId.toString()}-${trancheId.toString()} token: ${tokenAddress}`)
+  await createTrancheTrackerDatasource(({ address: tokenAddress }))
+}
+
+export const handleEvmTransfer = errorHandler(_handleEvmTransfer)
+async function _handleEvmTransfer(event: TransferLog): Promise<void> {
+  const [ from, to, amount ] = event.args
+  logger.info(`Transfer ${from}-${to} of ${amount.toString()}`)
 }
