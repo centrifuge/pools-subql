126af80d83feb216e08ef108b2b853f3a200ee68
Adam Stox <adamstox@protonmail.com>
2024-02-07 17:39:17 -0500

    fix: move handleEvmBlock to its own file and rename to handleEthBlock

--- src/mappings/handlers/evmHandlers.ts
+++ src/mappings/handlers/evmHandlers.ts
@@ -1,6 +1,4 @@
-import { Loan, LoanStatus, createTrancheTrackerDatasource } from '../../types'
-import { EthereumBlock } from '@subql/types-ethereum'
-import { DAIMainnetAddress, tinlakePools } from '../../config'
+import { createTrancheTrackerDatasource } from '../../types'
 import { errorHandler } from '../../helpers/errorHandler'
 import { DeployTrancheLog } from '../../types/abi-interfaces/PoolManagerAbi'
 import { TransferLog } from '../../types/abi-interfaces/Erc20Abi'
@@ -11,87 +9,12 @@ import { InvestorTransactionData, InvestorTransactionService } from '../services
 import { CurrencyService } from '../services/currencyService'
 import { BlockchainService } from '../services/blockchainService'
 import { CurrencyBalanceService } from '../services/currencyBalanceService'
-import {
-  InvestmentManagerAbi__factory,
-  PoolManagerAbi__factory,
-  ShelfAbi__factory,
-  NavfeedAbi__factory,
-  ReserveAbi__factory,
-  PileAbi__factory,
-} from '../../types/contracts'
+import { InvestmentManagerAbi__factory, PoolManagerAbi__factory } from '../../types/contracts'
 import { Provider } from '@ethersproject/providers'
 import { TrancheBalanceService } from '../services/trancheBalanceService'
-import { TimekeeperService, getPeriodStart } from '../../helpers/timekeeperService'
-import { LoanService } from '../services/loanService'
-import { evmStateSnapshotter } from '../../helpers/stateSnapshot'
 
 const ethApi = api as unknown as Provider
 //const networkPromise = typeof ethApi.getNetwork === 'function' ? ethApi.getNetwork() : null
-
-const timekeeper = TimekeeperService.init()
-
-export const handleEvmBlock = errorHandler(_handleEvmBlock)
-async function _handleEvmBlock(block: EthereumBlock): Promise<void> {
-  const date = new Date(Number(block.timestamp) * 1000)
-  const blockNumber = block.number
-  const newPeriod = (await timekeeper).processBlock(date)
-  const blockPeriodStart = getPeriodStart(date)
-
-  const blockchain = await BlockchainService.getOrInit('1')
-  const currency = await CurrencyService.getOrInitEvm(blockchain.id, DAIMainnetAddress)
-
-  if (newPeriod) {
-    logger.info(`It's a new period on EVM block ${blockNumber}: ${date.toISOString()}`)
-
-    // update pool states
-    for (const tinlakePool of tinlakePools) {
-      let pool
-      if (block.number >= tinlakePool.startBlock) {
-        pool = await PoolService.getOrSeed(tinlakePool.id)
-        if (block.number >= tinlakePool.startBlock && pool.totalReserve == null) {
-          pool.totalReserve = BigInt(0)
-          pool.portfolioValuation = BigInt(0)
-          pool.currency
-          pool.isActive = false
-          pool.currencyId = currency.id
-          await pool.save()
-          logger.info(`Initializing pool ${tinlakePool.id}`)
-        }
-        const latestNavFeed = getLatestContract(tinlakePool.navFeed, blockNumber)
-        if (latestNavFeed) {
-          const navFeedContract = NavfeedAbi__factory.connect(latestNavFeed.address, api as unknown as Provider)
-          pool.portfolioValuation = (await navFeedContract.currentNAV()).toBigInt()
-          await pool.save()
-          logger.info(`Updating pool ${tinlakePool.id} with portfolioValuation: ${pool.portfolioValuation}`)
-        }
-        const latestReserve = getLatestContract(tinlakePool.reserve, blockNumber)
-        if (latestReserve) {
-          const reserveContract = ReserveAbi__factory.connect(latestReserve.address, ethApi)
-          pool.totalReserve = (await reserveContract.totalBalance()).toBigInt()
-          await pool.save()
-          logger.info(`Updating pool ${tinlakePool.id} with totalReserve: ${pool.totalReserve}`)
-        }
-        // Update loans
-        if (latestNavFeed) {
-          updateLoans(
-            tinlakePool.id,
-            date,
-            tinlakePool.shelf[0].address,
-            tinlakePool.pile[0].address,
-            latestNavFeed.address
-          )
-        }
-      }
-    }
-
-    // Take snapshots
-    await evmStateSnapshotter('Pool', 'PoolSnapshot', block, 'poolId')
-    await evmStateSnapshotter('Loan', 'LoanSnapshot', block, 'loanId', 'isActive', true)
-
-    //Update tracking of period and continue
-    await (await timekeeper).update(blockPeriodStart)
-  }
-}
 
 export const handleEvmDeployTranche = errorHandler(_handleEvmDeployTranche)
 async function _handleEvmDeployTranche(event: DeployTrancheLog): Promise<void> {
@@ -201,65 +124,4 @@ async function _handleEvmTransfer(event: TransferLog): Promise<void> {
     const txOut = InvestorTransactionService.transferOut({ ...orderData, address: fromAccount.id })
     await txOut.save()
   }
-}
-
-async function updateLoans(poolId: string, blockDate: Date, shelf: string, pile: string, navFeed: string) {
-  let existingLoans = await LoanService.getByPoolId(poolId)
-  let loanIndex = existingLoans.length || 1
-  const contractLoans = []
-  // eslint-disable-next-line
-  while (true) {
-    const shelfContract = ShelfAbi__factory.connect(shelf, ethApi)
-    const response = await shelfContract.token(loanIndex)
-    if (Number(response.nft) === 0) {
-      // no more loans
-      break
-    }
-    contractLoans.push(loanIndex)
-    loanIndex++
-  }
-  const newLoans = contractLoans.filter((loanIndex) => !existingLoans.includes(loanIndex))
-  // create new loans
-  for (const loanIndex of newLoans) {
-    const loan = new Loan(`${poolId}-${loanIndex}`, blockDate, poolId, true, LoanStatus.CREATED)
-
-    const navFeedContract = NavfeedAbi__factory.connect(navFeed, ethApi)
-    const nftId = await navFeedContract['nftID(uint256)'](loanIndex)
-    const maturityDate = await navFeedContract.maturityDate(nftId)
-    loan.actualMaturityDate = new Date(Number(maturityDate) * 1000)
-    loan.save()
-  }
-
-  // update all loans
-  existingLoans = await LoanService.getByPoolId(poolId)
-  for (const loan of existingLoans) {
-    const shelfContract = ShelfAbi__factory.connect(shelf, ethApi)
-    const loanIndex = loan.id.split('-')[1]
-    const nftLocked = await shelfContract.nftLocked(loanIndex)
-    if (!nftLocked) {
-      loan.isActive = false
-      loan.status = LoanStatus.CLOSED
-      loan.save()
-    }
-    const pileContract = PileAbi__factory.connect(pile, ethApi)
-    const prevDebt = loan.outstandingDebt
-    const debt = await pileContract.debt(loanIndex)
-    loan.outstandingDebt = debt.toBigInt()
-    const rateGroup = await pileContract.loanRates(loanIndex)
-    const rates = await pileContract.rates(rateGroup)
-    loan.interestRatePerSec = rates.ratePerSecond.toBigInt()
-
-    if (prevDebt > loan.outstandingDebt) {
-      loan.repaidAmountByPeriod = prevDebt - loan.outstandingDebt
-    }
-    loan.save()
-  }
-}
-
-function getLatestContract(contractArray, blockNumber) {
-  return contractArray.reduce(
-    (prev, current) =>
-      current.startBlock <= blockNumber && current.startBlock > (prev?.startBlock || 0) ? current : prev,
-    null
-  )
 }
