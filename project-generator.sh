faa6ea33e14fbe38c33ef1a3706830de3ec682bc
Filippo Fontana <filippo@embriotech.ch>
2024-02-13 13:10:41 +0100

    ci: use sh syntax

--- project-generator.sh
+++ project-generator.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 cat > subquery-multichain.yaml <<"EOF"
 specVersion: 1.0.0
 query:
@@ -12,7 +12,7 @@ yq ". *=d load(\"chains-cfg/${CHAIN_ID}.yaml\")" chains-cfg/_root.yaml > project
 
 subdirs=$(find chains-evm -mindepth 1 -maxdepth 1 -type d)
 for subdir in $subdirs; do
-    if [[ -e "$subdir/${CHAIN_ID}.yaml" ]]; then
+    if [ -e "$subdir/${CHAIN_ID}.yaml" ]; then
       echo "Generating EVM config for ${subdir##*/}"
       yq ". *=d load(\"$subdir/${CHAIN_ID}.yaml\")" chains-evm/_root.yaml > project-${subdir##*/}.yaml
       echo "  - project-${subdir##*/}.yaml" >> subquery-multichain.yaml
