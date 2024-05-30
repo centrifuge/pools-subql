5f3c7d45e1cc4d8c1e29353d792434df63039cc3
Jeroen Offerijns <jeroen@offerijns.com>
2024-05-30 20:29:48 +0200

    fix: log

--- src/mappings/handlers/oracleHandlers.ts
+++ src/mappings/handlers/oracleHandlers.ts
@@ -6,6 +6,7 @@ import { OracleTransactionData, OracleTransactionService } from '../services/ora
 export const handleOracleFed = errorHandler(_handleOracleFed)
 async function _handleOracleFed(event: SubstrateEvent<OracleFedEvent>) {
   const [feeder, key, value] = event.event.data
+  logger.info(`Oracle feed: ${feeder.toString()} key: ${key.toString()} value: ${value.toString()}`)
   logger.info(`Oracle feed: ${feeder.toString()} key: ${key.isin.toString()} value: ${value.toString()}`)
 
   const oracleTxData: OracleTransactionData = {
