feeb31fec9aceb1773b5a7d001f43f0b9fd9b1d0
Jeroen Offerijns <jeroen@offerijns.com>
2024-05-30 10:49:55 +0200

    feat: set up oracle service and handler

--- src/mappings/handlers/oracleHandlers.ts
+++ src/mappings/handlers/oracleHandlers.ts
@@ -1,9 +1,19 @@
 import { SubstrateEvent } from '@subql/types'
 import { OracleFedEvent } from '../../helpers/types'
 import { errorHandler } from '../../helpers/errorHandler'
+import { OracleTransactionData, OracleTransactionService } from '../services/oracleTransactionService'
 
 export const handleOracleFed = errorHandler(_handleOracleFed)
 async function _handleOracleFed(event: SubstrateEvent<OracleFedEvent>) {
   const [feeder, key, value] = event.event.data
   logger.info(`Oracle feed: ${feeder.toString()} key: ${key.toString()} value: ${value.toString()}`)
+
+  const oracleTxData: OracleTransactionData = {
+    hash: event.extrinsic.extrinsic.hash.toString(),
+    timestamp: event.block.timestamp,
+    key: key.toString(),
+    value: value.toBigInt(),
+  }
+
+  OracleTransactionService.init(oracleTxData)
 }
---
+++ src/mappings/services/oracleTransactionService.ts
@@ -0,0 +1,25 @@
+import { OracleTransaction } from '../../types'
+
+export interface OracleTransactionData {
+  readonly hash: string
+  readonly timestamp: Date
+  readonly key: string
+  readonly value?: bigint
+}
+
+export class OracleTransactionService extends OracleTransaction {
+  static init = (data: OracleTransactionData) => {
+    const tx = new this(
+      `${data.hash}-${data.timestamp.toString()}-${key.toString()}`,
+      data.timestamp,
+      data.key.toString(),
+      data.value.toBigInt()
+    )
+
+    tx.timestamp = data.timestamp ?? null
+    tx.key = data.key ?? null
+    tx.value = data.value ?? null
+
+    return tx
+  }
+}
