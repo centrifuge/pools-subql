07a8fae172090784a1edcc729c62fcadacb10a5b
Filippo Fontana <filippo@embrio.tech>
2024-06-12 14:12:26 +0200

    feat: track oracle transactions Fixes #161

--- src/mappings/handlers/oracleHandlers.ts
+++ src/mappings/handlers/oracleHandlers.ts
@@ -6,23 +6,33 @@ import { OracleTransactionData, OracleTransactionService } from '../services/ora
 export const handleOracleFed = errorHandler(_handleOracleFed)
 async function _handleOracleFed(event: SubstrateEvent<OracleFedEvent>) {
   const [feeder, key, value] = event.event.data
-  const formattedKey = key.asIsin
-  logger.info(`Oracle feed: ${feeder.toString()} key: ${hex2a(formattedKey)} value: ${value.toString()}`)
+
+  let formattedKey: string
+
+  switch (key.type) {
+    case 'Isin': {
+      formattedKey = key.asIsin.toUtf8()
+      break
+    }
+    case 'PoolLoanId': {
+      const [poolId, loanId] = key.asPoolLoanId.map((item) => item.toString(10))
+      formattedKey = `${poolId}-${loanId}`
+      break
+    }
+    default:
+      logger.warn(`Oracle feed: ${feeder.toString()} key: ${formattedKey} value: ${value.toString()}`)
+      return
+  }
+
+  logger.info(`Oracle feeder: ${feeder.toString()} key: ${formattedKey} value: ${value.toString()}`)
 
   const oracleTxData: OracleTransactionData = {
     hash: event.extrinsic.extrinsic.hash.toString(),
     timestamp: event.block.timestamp,
-    key: hex2a(formattedKey),
+    key: formattedKey,
     value: value.toBigInt(),
   }
 
   const oracleTx = OracleTransactionService.init(oracleTxData)
   await oracleTx.save()
-}
-
-const hex2a = (hexx: string) => {
-  const hex = hexx.toString()
-  let str = ''
-  for (let i = 0; i < hex.length; i += 2) str += String.fromCharCode(parseInt(hex.substr(i, 2), 16))
-  return str
 }
