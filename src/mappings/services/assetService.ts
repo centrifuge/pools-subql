e8dfdf2dcd0408763e972926419d5ce0a2e2e8d1
Jeroen Offerijns <jeroen@offerijns.com>
2024-06-12 15:40:04 +0200

    fix: init

--- src/mappings/services/assetService.ts
+++ src/mappings/services/assetService.ts
@@ -43,6 +43,7 @@ export class AssetService extends Asset {
     asset.presentValue = BigInt(0)
     asset.outstandingQuantity = BigInt(0)
     asset.currentPrice = BigInt(0)
+    asset.periodPrice = BigInt(0)
     asset.notional = BigInt(0)
     asset.writeOffPercentage = BigInt(0)
     asset.totalBorrowed = BigInt(0)
